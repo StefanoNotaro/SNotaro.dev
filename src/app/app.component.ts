7bb485f74b2bb208ccbc3d93fc5da1deec21cf40
Stefano Notaro <stefano.notaro@gmail.com>
2023-01-30 14:15:21 +0100

    Init title service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TitleService } from './core/services/title.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SNotaro.dev';
+  
+  constructor(
+    private titleService: TitleService, // Initialize title service
+  ) {
+  }
 }
---
+++ src/app/core/services/title.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TitleService {
+  private suffix: string = ' | Portfolio';
+
+  constructor(
+    private router: Router,
+    private title: Title
+  ){
+    this.router.events
+    .pipe( filter(event => event instanceof NavigationEnd) )
+    .subscribe(x => {
+      console.log(x);
+      this.title.setTitle(`Stefano Notaro${this.suffix}`);
+    });
+  }
+}
