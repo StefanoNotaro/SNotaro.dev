13e26e2f6ee6c5a28f5b21ff91d5013a0bf1e0a1
Stefano Notaro <stefano.notaro@gmail.com>
2023-01-31 12:42:53 +0100

    heading responsivnes

--- src/environments/environment.core.ts
+++ src/environments/environment.core.ts
@@ -1,4 +1,5 @@
 export const coreEnvironment = {
     personalEmail: 'stefano.notaro@gmail.com',
     linkedinHref: 'https://www.linkedin.com/in/stefano-notaro-francesco-gauthier',
+    githubHref: 'https://github.com/StefanoNotaro',
 }
