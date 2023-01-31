395cfa1de29087c769340e12a20e2b07cc4c181e
Stefano Notaro <stefano.notaro@gmail.com>
2023-01-31 01:33:17 +0100

    init environment files

---
+++ src/environments/environment.core.ts
@@ -0,0 +1,3 @@
+export const coreEnvironment = {
+    personalEmail: 'stefano.notaro@gmail.com'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { coreEnvironment } from './environment.core';
+
+export const environment = {
+    production: false,
+    ...coreEnvironment
+}
