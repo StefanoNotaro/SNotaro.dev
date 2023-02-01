26a49c2408819f4304f0d98514b6f85a51dd3756
Stefano Notaro <stefano.notaro@gmail.com>
2023-02-01 23:18:04 +0100

    page theming

--- src/app/share/components/footer/footer.component.ts
+++ src/app/share/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-footer',
@@ -9,5 +10,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  public environment = environment;
 
+  public get currentYear(): number { return new Date().getFullYear(); }
 }
--- src/environments/environment.core.ts
+++ src/environments/environment.core.ts
@@ -1,4 +1,5 @@
 export const coreEnvironment = {
+    githubProjectHref: 'https://github.com/StefanoNotaro/SNotaro.dev',
     personalEmail: 'stefano.notaro+portfolio@gmail.com',
     personalEmailDisplay: 'stefano.notaro@gmail.com',
     whatsappHref: 'https://wa.me/34664547111?text=Hey! I just saw your portfolio page.',
