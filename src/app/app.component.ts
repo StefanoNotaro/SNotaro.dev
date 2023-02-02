18e1b2c2f8fbff06ab90f510ac8d3a5ca4174b81
Stefano Notaro <stefano.notaro@gmail.com>
2023-02-02 01:24:45 +0100

    mobile heading and scroll heading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { TitleService } from './share/services/title.service';
 
 @Component({
@@ -7,14 +7,22 @@ import { TitleService } from './share/services/title.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'SNotaro.dev';
-  
+  public headerHeight = 0;
+
   constructor(
     private titleService: TitleService,
+    private changeDetectorRef: ChangeDetectorRef
   ) {
   }
 
   public ngOnInit(): void {
     this.titleService.subscribe();
+  }
+
+  public onHeaderHeightChanged(headerHeight: number): void {
+    if (this.headerHeight !== headerHeight) {
+      this.headerHeight = headerHeight;
+      this.changeDetectorRef.detectChanges();
+    }
   }
 }
